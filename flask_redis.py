ad5b9b66a4f3f375bfd38293e32097edd3153e2f
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2015-10-20 22:39:54 +0700

    store request data in redis

--- flask_redis.py
+++ flask_redis.py
@@ -1,14 +1,20 @@
+import os
 from flask import Flask
 from flask import request
+import redis
+
 app = Flask(__name__)
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 
 @app.route('/<key>', methods=['GET', 'POST'])
 def redis_key(key):
     if request.method == 'POST':
-        return 'POST'
+        r.set(key, request.data)
+        return request.data
     else:
-        return 'GET'
+        data = r.get(key)
+        return data
 
 
 if __name__ == '__main__':
